5be9bf5844a7d722a8e57259f918fe533d93662c
Kim Minji <vvzmsqufvv@gmail.com>
2022-11-06 21:17:34 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -0,0 +1,9 @@
+// js
+function solution(numbers, num1, num2) {
+    return numbers.splice(num1, num2 - num1 + 1);
+}
+
+
+// py
+// def solution(numbers, num1, num2):
+//     return numbers[num1:num2+1]
