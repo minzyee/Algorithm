305f073dd7ae08f1f316b9b863667973e2a22b68
Kim Minji <vvzmsqufvv@gmail.com>
2022-11-29 09:25:58 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -2,3 +2,11 @@ function solution(str1, str2) {
     var answer = 0;
     return str1.includes(str2) ? 1: 2;
 }
+
+
+function solution(str1, str2) {
+    if (str1.indexOf(str2) === -1){
+        return 2;
+    }
+    return 1;
+}
