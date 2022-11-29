9851548a11b8e94068bfe28ea324369c4abf8752
Kim Minji <vvzmsqufvv@gmail.com>
2022-11-29 13:46:26 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -0,0 +1,11 @@
+function solution(lines) {
+    // 100이 0, 101은 1
+    // 99는 -1, 98은 -2
+    let line = Array(201).fill(0); // 0 포함 201개
+    for (let [i, j] of lines) {
+        for (; i < j; i++) {
+            line[100 + i]++;
+        }
+    }
+    return line.filter((v) => v >= 2).length;
+}
