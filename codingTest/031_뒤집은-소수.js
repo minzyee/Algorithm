8376e884d5e1f3ae01db5f4920de5ddad8352cf4
minzyee <vvzmsqufvv@gmail.com>
2023-05-23 15:13:23 +0900

    refactor: 소수의 조건 파악 후, 소수 판별 함수를 만든다. num%10은 나머지로, 일의자리를 구할 수 있다. num/10은 몫을 구할 수 있음을 활용하기

--- codingTest/031_뒤집은-소수.js
+++ codingTest/031_뒤집은-소수.js
@@ -14,11 +14,11 @@ let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
 function isPrime(num) {
 	// 1과 자기 자신 이외에 나누어 떨어지는 수가 있으면 소수가 아니기 때문에 false를 반환한다.
 	if (num === 1) return false;
-
-	for (let i = 2; i <= parseInt(num / 2); i++) {
-		if (num % i === 0) return false;
+	// Math.sqrt(num)으로 num의 제곱근 까지만 반복되게 해도 된다.
+	for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+		if (num % i === 0) return false; // 1이 아닌 num을 2부터 num의 절반까지 반복문 돌렸을 때, 0이 나오면 소수가 아니다.
 	}
-	return true;
+	return true; // 위의 과정 중 return false 당하지 않고, true까지 도달한 num은 소수라고 판별한다.
 }
 
 function solution(arr) {
@@ -31,29 +31,35 @@ function solution(arr) {
 
 	return answer;
 }
-
-console.log(solution(arr));
+console.log(solution(arr)); // [23, 2, 73, 2, 3]
 
 // 해답
+// 소수를 판별해주는 함수
 function isPrime1(num) {
-	if (num === 1) return false;
-	for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
-		if (num % i === 0) return false;
+	// 1과 자기 자신 이외에 나누어 떨어지는 수가 있으면 소수가 아니기 때문에 false를 반환한다.
+	if (num === 1) return false; // 1은 소수가 아니다.
+	// Math.sqrt(num)으로 num의 제곱근 까지만 반복되게 해도 된다.
+	// num이 1이 아니라면, for문 실행한다.
+	// num / 2;를 한 이유는 어떤 숫자든 1과 자기자신을 뺀것 중 약수의 가장 큰 값은 자기자신의 절반이다.
+	for (let i = 2; i <= parseInt(num / 2); i++) {
+		if (num % i === 0) return false; // 1이 아닌 num을 2부터 num의 절반까지 반복문 돌렸을 때, 0이 나오면 소수가 아니다.
 	}
-	return true;
+	return true; // 위의 과정 중 return false 당하지 않고, true까지 도달한 num은 소수라고 판별한다.
 }
+
 function solution1(arr) {
 	let answer = [];
 	for (let x of arr) {
-		let res = 0;
+		let result = 0; // result는 완성된 뒤집은 숫자를 담는 변수
+		// x가 0이 되면 멈춘다. 현재 while문은 각 원소를 뒤집어주고있다.
 		while (x) {
-			let t = x % 10;
-			res = res * 10 + t;
-			x = parseInt(x / 10);
+			let t = x % 10; // x의 나머지(즉, 일의자리)
+			result = result * 10 + t; // result에는 result * 10 + 일의자리.
+			x = parseInt(x / 10); // x의 몫. 이것이 0이되면 while문은 종료된다.
 		}
-		if (isPrime1(res)) answer.push(res);
+		// isprime1함수에 뒤집은 숫자를 매개변수를 통해 전달한 뒤, 소수이면 answer에 그 result를 push한다.
+		if (isPrime1(result)) answer.push(result);
 	}
 	return answer;
 }
-
-console.log(solution1(arr));
+console.log(solution1(arr)); // [23, 2, 73, 2, 3]
