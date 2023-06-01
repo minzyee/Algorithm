8cdb5c7ece76323234d0f6e656ee65b293e1cd57
minzyee <vvzmsqufvv@gmail.com>
2023-06-01 22:50:23 +0900

    solve: 스택 문제풀이

---
+++ codingTest/자료구조(스택,큐)/01_올바른괄호(스택).js
@@ -0,0 +1,21 @@
+// 문제
+// 괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
+// (())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.
+
+let a = "(()(()))(()";
+function solution(s) {
+	let answer = "YES";
+	stack = [];
+
+	for (let x of s) {
+		if (x === "(") stack.push(x);
+		else {
+			if (stack.length === 0) return "NO";
+			stack.pop();
+		}
+		if (stack.length > 0) return "NO";
+		return answer;
+	}
+}
+
+console.log(solution(a));
