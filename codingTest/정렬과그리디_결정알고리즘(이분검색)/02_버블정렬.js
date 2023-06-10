4159cf034d8a7fb2440463a4edeb1046a0261853
minzyee <vvzmsqufvv@gmail.com>
2023-06-10 23:18:14 +0900

    solve: 버블정렬 문제풀이

---
+++ codingTest/정렬과그리디_결정알고리즘(이분검색)/02_버블정렬.js
@@ -0,0 +1,18 @@
+// 문제
+// N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요. 정렬하는 방법은 버블정렬입니다.
+
+let arr = [13, 5, 11, 7, 23, 15];
+
+function solution(arr) {
+	let answer = arr;
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+			}
+		}
+	}
+	return answer;
+}
+
+console.log(solution(arr));
